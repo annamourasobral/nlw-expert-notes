f75e0267da6227c9be7fe8a183d69c4b716cc05e
annamourasobral <anna.moura@qatium.com>
2024-02-06 19:31:37 +0100

    translate to english

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,10 +1,10 @@
 export function NewNoteCard() {
     return (
       <div className="rounded-md bg-slate-700 p-5 space-y-3">
-        <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
+        <span className="text-sm font-medium text-slate-200">Add note</span>
   
         <p className="text-sm leading-6 text-slate-400">
-          Grave uma nota em áudio que será convertida para texto automaticamente.
+        Record an audio note that will automatically be converted to text.
         </p>
       </div>
     );
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,7 +1,7 @@
 export function NoteCard() {
     return (
       <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-        <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+        <span className="text-sm font-medium text-slate-300">2 days ago</span>
   
         <p className="text-sm leading-6 text-slate-400">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus commodi
